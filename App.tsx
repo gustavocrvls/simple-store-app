884e7d9349bf834b56c995f53e81d6aba8c63fdb
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2022-03-16 21:36:49 -0300

    feat(routes): create routes component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { Home } from './src/screens/home';
-
-const Stack = createNativeStackNavigator();
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Routes />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
